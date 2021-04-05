b7494ff05031f9cc023fdcedfe299c79ce208736
Nunu24 <mar.szym0123@gmail.com>
2021-04-05 15:31:28 +0200

    mailer typo fix

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
-class ContactMailer < ActionMailer::BasicObject
+class ContactMailer < ActionMailer::Base
     default to: 'xuannu233@gmail.com'
-    
+
     def contact_email(name, email, body)
         @name = name
         @email = email
