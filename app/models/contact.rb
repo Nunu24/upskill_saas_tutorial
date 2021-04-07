10226adcd35e8e95022b2a313e00f2c7dc4351db
Nunu24 <mar.szym0123@gmail.com>
2021-04-07 12:40:43 +0200

    added comments

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+    # Contact form validations
     validates :name, presence: true
     validates :email, presence: true, length: {minimum:5, maximum:35}    
     validates :comments, presence: true
